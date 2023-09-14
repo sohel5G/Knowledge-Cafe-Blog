8da04270cc11eb1f7cef21fbff74e8d7147c6ab5
sohel5G <105277301+sohel5G@users.noreply.github.com>
2023-09-14 20:31:22 +0600

    All done have fun

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
     return (
         <div className='flex justify-between items-center py-5'>
             <h1 className="text-3xl font-bold">Knowledge Cafe</h1>
-            <img src={profile} alt="Blog Header Profile picture" />
+            <img className='w-10' src={profile} alt="Blog Header Profile picture" />
         </div>
     )
 }
